fab77ae86dbf0ed554ff722d0416205da8a97466
Maria Tkacheva <tkachevamasha35@yandex.ru>
2023-05-27 19:53:24 +0300

    division

--- src/App.js
+++ src/App.js
@@ -7,31 +7,79 @@ import imgMinus from './img/minus.png'
 import imgPlus from './img/plus.png'
 
 function App() {
-  const [currentVal, SetCurrentVal] = useState('0')
+
+  const [currentVal, setCurrentVal] = useState(0)
+  const [AC, setAC] = useState('AC')
+  const[curOperation, setCurOperation] =useState()
+  const[newWindow, setnewWindow] =useState(false)
+  const [firstOperand, setFirstOperand] = useState('')
+
+  function addNum(e){
+    setAC('C')
+    
+    if (currentVal === 0 || newWindow){
+      setnewWindow(false)
+      setCurrentVal(e.currentTarget.textContent)
+    }else{
+      if (String(currentVal).length<9){
+        setCurrentVal(String(currentVal) + String(e.currentTarget.textContent))
+      }
+    }
+  }
+
+  function clearWindow(e){
+    setAC('AC')
+    setCurrentVal(0)
+    setCurOperation()
+  }
+
+  function processingOperation(val){
+    setAC('C')
+    setFirstOperand(currentVal)
+    setCurOperation(val)
+    setnewWindow(true)
+  }
+
+  function result(){
+    let secondOperandResult = currentVal
+    let firstOperandResult = firstOperand
+    let currentResult
+    
+    firstOperandResult = Number(String(firstOperand).replace(',','.'))
+    secondOperandResult = Number(String(secondOperandResult).replace(',','.'))
+    if (curOperation ==="division"){
+      currentResult = firstOperandResult/secondOperandResult
+    }
+
+    setCurrentVal(String(Math.round(currentResult*1000000)/1000000).replace('.',','))
+  }
+
   return (
     <div className="App">
       <main className='main'>
         <div className='main_window'>{currentVal}</div>
         <div className='main_btns'>
-          <div className='main_item-btn'>AC</div>
+          <div className='main_item-btn'onClick={(e)=>clearWindow(e)}>{AC}</div>
           <div className='main_item-btn'><img src={imgSignBold} alt=''/></div>
-          <div className='main_item-btn'>%</div>
-          <div className='main_item-btn'><img src={imgDivision} alt=''/></div>
-          <div className='main_item-btn'>7</div>
-          <div className='main_item-btn'>8</div>
-          <div className='main_item-btn'>9</div>
+          <div className='main_item-btn' onClick={()=>processingOperation('persent')}>%</div>
+          <div className='main_item-btn' onClick={()=>processingOperation('division')}><img src={imgDivision} alt=''/></div>
+          <div className='main_item-btn' onClick={(e)=>addNum(e)}>7</div>
+          <div className='main_item-btn' onClick={(e)=>addNum(e)}>8</div>
+          <div className='main_item-btn' onClick={(e)=>addNum(e)}>9</div>
           <div className='main_item-btn'><img src={imgMultiply} alt=''/></div>
-          <div className='main_item-btn'>4</div>
-          <div className='main_item-btn'>5</div>
-          <div className='main_item-btn'>6</div>
+          <div className='main_item-btn' onClick={(e)=>addNum(e)}>4</div>
+          <div className='main_item-btn' onClick={(e)=>addNum(e)}>5</div>
+          <div className='main_item-btn' onClick={(e)=>addNum(e)}>6</div>
           <div className='main_item-btn'><img src={imgMinus} alt=''/></div>
-          <div className='main_item-btn'>1</div>
-          <div className='main_item-btn'>2</div>
-          <div className='main_item-btn'>3</div>
+          <div className='main_item-btn' onClick={(e)=>addNum(e)}>1</div>
+          <div className='main_item-btn' onClick={(e)=>addNum(e)}>2</div>
+          <div className='main_item-btn' onClick={(e)=>addNum(e)}>3</div>
           <div className='main_item-btn'><img src={imgPlus} alt=''/></div>
-          <div className='main_item-btn'>0</div>
-          <div className='main_item-btn'>,</div>
-          <div className='main_item-btn'>=</div>
+          <div className='main_item-btn' onClick={(e)=>addNum(e)}>0</div>
+          <div className='main_item-btn' onClick={(e)=>addNum(e)}>,</div>
+          <div className='main_item-btn' onClick={()=>{
+            result()
+          }}>=</div>
         </div>
       </main>
     </div>
